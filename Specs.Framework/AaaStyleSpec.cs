4ca32fc8c366c296b96d20d1b028188a1d9255ef
Olivier.Costa <olivier.costa@aegisoft.be>
2013-08-31 16:22:07 +0200

    Initial commit

---
+++ Specs.Framework/AaaStyleSpec.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace AEGIS.Specs.Framework
+{
+    public abstract class AaaStyleSpec
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Arrange();
+            Act();
+        }
+
+        protected virtual void Arrange() { } //override to prepare system-under-test
+        protected virtual void Act() { }     //override to trigger system-under-test
+
+    }
+}
